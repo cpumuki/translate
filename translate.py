15eb112dadde5ef31eefa7a440551f22b3c80887
ckishimo <ckishimo@gmail.com>
2018-05-23 20:30:42 +0200

    add interface module

--- translate.py
+++ translate.py
@@ -9,39 +9,7 @@ from bgp import BGP
 from lag import LAG
 from vlan import VLAN
 from ve import VE
-
-class INTERFACE(object):
-    """ Creates a physical INTERFACE object """
-    def __init__(self, lines):
-        self.port = ""
-        self.name = ""
-        self.enabled = 0
-        self.interval = 30
-        self.flow_control = 0
-        self.link_fault = 0
-        for l in lines:
-            r1 = re.match("interface ethernet (\S+)", l)
-            r2 = re.match("\s+port-name (.*)", l)
-            r3 = re.match("\s+load-interval (.*)", l)
-            if r1:
-                self.port = r1.group(1)
-            elif r2:
-                self.name = r2.group(1)
-            elif r3:
-                self.interval = r3.group(1)
-            else:
-                if "link-fault-signaling" in l:
-                    self.link_fault = 1
-                elif "no flow-control" in l:
-                    self.flow_control = 0
-                elif "enable" in l:
-                    self.enabled = 1
-                else: 
-                    # print("* Warning line skipped: %s" % l.strip("\n"))
-                    pass
-
-    def __repr__(self):
-        return "interface %s (%s)"  % (self.port, self.name)
+from interface import INTERFACE
 
 class SNMP(object):
     def __init__(self,lines):
